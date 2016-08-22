4a710b0ae7baf75efa5a784883400af57cc096ac
terry <2654126245@qq.com>
2016-08-22 10:14:11 +0800

    前台显示添加的新闻

--- src/Application/Ming/Controller/NewsController.class.php
+++ src/Application/Ming/Controller/NewsController.class.php
@@ -5,15 +5,51 @@ namespace Ming\Controller;
 class NewsController extends BaseController
 {
 	public function news(){
+		$this->set_news();
 		$this->display();
 	}
 	
 	public function internet(){
+		$this->set_internet_news();
 		$this->display();
 	}
 	
 	public function company(){
+		$this->set_company_news();
 		$this->display();
 	}
 	
+	public function detail(){
+		$id = I('id');
+		$list = D('News')->where("id=$id")->find();
+		\Think\Log::record('list=====++===>'. json_encode($list));
+		$list["date"]=date("Y-m-d ", $list["date"]);
+		$this->assign('news', $list);
+		$this->display('178');
+	}
+
+    public function set_company_news(){
+		$list = D('news')->where("type = 'company'")->select();
+		\Think\Log::record('list=====+===>'. json_encode($list));
+		foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["date"]);
+		}
+		$this->assign('news', $list);
+    }
+
+    public function set_internet_news(){
+		$list = D('news')->where("type = 'internet'")->select();
+		foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["date"]);
+		}
+		$this->assign('news', $list);
+    }
+
+    public function set_news(){
+		$list = D('news')->where("type = 'news'")->select();
+		foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["date"]);
+		}
+		$this->assign('news', $list);
+    }
 }
