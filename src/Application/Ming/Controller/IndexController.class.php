3d80e82104e8712ecb5c35484114b441b7de6f5b
LuckStone <shaojun_d@126.com>
2016-08-26 20:55:38 +0800

    合入修改

--- src/Application/Ming/Controller/IndexController.class.php
+++ src/Application/Ming/Controller/IndexController.class.php
@@ -9,6 +9,12 @@ class IndexController extends BaseController {
 	}
 	
     public function index(){
+    	$company_news = array();
+    	$internet_news = array();
+    	$news = array();
+    	$this->assign('company_news', $company_news);
+    	$this->assign('internet_news', $list);
+    	$this->assign('news', $list);
         $this->display();
     }
 }
