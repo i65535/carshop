90ef77483f0c0ca4afa2a745891e78dc1df3cdf3
LuckStone <shaojun_d@126.com>
2016-08-29 21:01:58 +0800

    实现新闻分页

--- src/Application/Ming/Controller/NewsController.class.php
+++ src/Application/Ming/Controller/NewsController.class.php
@@ -4,54 +4,97 @@ namespace Ming\Controller;
 
 class NewsController extends BaseController
 {
+
+	public function __construct(){
+        parent::__construct();
+        $this->instance = D('News');
+    }
+
 	public function news(){
-		$this->set_news();
+
+		$offset = $this->pageLimit(U('news', array('page'=>'{page}','category'=>'news')), 5);
+        $total = $this->get_total_count("category='news'");
+        $this->assign('page', $this->pageShow($total));
+
+        $list = $this->instance->where("category='news'")->order('id desc')->limit($offset)->select();
+        foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
+		}
+
+        $this->assign('news', $list);
+
 		$this->display();
 	}
 	
 	public function internet(){
-		$this->set_internet_news();
+		$offset = $this->pageLimit(U('internet', array('page'=>'{page}','category'=>'internet')), 5);
+        $total = $this->get_total_count("category='internet'");
+        $this->assign('page', $this->pageShow($total));
+
+        $list = $this->instance->where("category='internet'")->order('id desc')->limit($offset)->select();
+        foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
+		}
+
+        $this->assign('news', $list);
+
 		$this->display();
 	}
 	
 	public function company(){
-		$this->set_company_news();
+		$offset = $this->pageLimit(U('company', array('page'=>'{page}','category'=>'company')), 2);
+        $total = $this->get_total_count("category='company'");
+        $this->assign('page', $this->pageShow($total));
+
+        $list = $this->instance->where("category='company'")->order('id desc')->limit($offset)->select();
+        foreach ($list as $key => $value) {
+			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
+		}
+
+        $this->assign('news', $list);
+
 		$this->display();
 	}
 	
 	public function detail(){
 		$id = I('id');
-		$list = D('News')->where("id=$id")->find();
-		\Think\Log::record('list=====++===>'. json_encode($list));
-		$list["date"]=date("Y-m-d ", $list["create_time"]);
-		$list["content"]=html_out($list["content"]);
-		$this->assign('news', $list);
+		$current = $this->instance->where("id=$id")->find();
+		$current["create_time"]=date("Y-m-d ", $current["create_time"]);
+		$current["content"]=html_out($current["content"]);
+		$this->assign('news', $current);
+
+		$pre = $this->instance->where("id<$id")->field('id, title')->order('id desc')->limit('0,1')->find();
+		$this->assign('pre', $pre);
+
+		$next = $this->instance->where("id>$id")->field('id, title')->order('id asc')->limit('0,1')->find();
+		$this->assign('next', $next);
+
 		$this->display('detail');
 	}
 
-    public function set_company_news(){
-		$list = D('news')->where("type = 'company'")->select();
-		\Think\Log::record('list=====+===>'. json_encode($list));
-		foreach ($list as $key => $value) {
-			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
-		}
-		
-		$this->assign('news', $list);
+    protected function get_total_count($condition='1=1'){
+    	return $this->instance->where($condition)->count();
     }
-
-    public function set_internet_news(){
-		$list = D('news')->where("type = 'internet'")->select();
-		foreach ($list as $key => $value) {
-			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
-		}
-		$this->assign('news', $list);
+    
+    // 获取分页查询limit
+    protected function pageLimit($url, $num = 10) {
+    	$url = str_replace(urlencode('{page}'), '{page}', $url);
+    	$page = is_object($this->pager ['obj']) ? $this->pager ['obj'] : new \Think\Pager();
+    	$cur_page = $page->getCurPage($url);
+    	$limit_start = ($cur_page - 1) * $num;
+    	$limit = $limit_start . ',' . $num;
+    	$this->pager = array(
+    			'obj' => $page,
+    			'url' => $url,
+    			'num' => $num,
+    			'cur_page' => $cur_page,
+    			'limit' => $limit
+    	);
+    	return $limit;
     }
-
-    public function set_news(){
-		$list = D('news')->where("type = 'news'")->select();
-		foreach ($list as $key => $value) {
-			$list[$key]["date"]=date("Y-m-d ", $list[$key]["create_time"]);
-		}
-		$this->assign('news', $list);
+    
+    // 分页结果显示
+    protected function pageShow($count) {
+    	return $this->pager ['obj']->show($this->pager ['url'], $count, $this->pager ['num']);
     }
 }
