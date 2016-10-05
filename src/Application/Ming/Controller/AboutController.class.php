5927190d7ff518b2bc95a00ded644cc4e4c3f4a2
terry <2654126245@qq.com>
2016-10-05 22:07:56 +0800

    about

--- src/Application/Ming/Controller/AboutController.class.php
+++ src/Application/Ming/Controller/AboutController.class.php
@@ -5,6 +5,10 @@ namespace Ming\Controller;
 class AboutController extends BaseController
 {
 	public function about(){
+		$list=D('article')->select();
+		$this->assign('title',html_out($list[0][title]));
+		$this->assign('content',html_out($list[0][content]));
+		\Think\Log::record('====list======>'.html_out($list[0][content]));
 		$this->display();
 	}
 	
